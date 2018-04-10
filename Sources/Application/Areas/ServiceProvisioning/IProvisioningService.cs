2bc3f0ee0407d84129bd6b7de32d2195438341be
Matthias Mueller <matthiasm@live.de>
2018-04-10 11:26:22 +0200

    Added ServiceProvisioning

---
+++ Sources/Application/Areas/ServiceProvisioning/IProvisioningService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mmu.Mlh.ApplicationExtensions.Areas.ServiceProvisioning
+{
+    public interface IProvisioningService
+    {
+        IReadOnlyCollection<T> GetAllServices<T>();
+
+        T GetService<T>();
+
+        object GetService(Type pluginType);
+    }
+}
---
+++ Sources/Application/Areas/ServiceProvisioning/ProvisioningServiceSingleton.cs
@@ -0,0 +1,12 @@
+﻿namespace Mmu.Mlh.ApplicationExtensions.Areas.ServiceProvisioning
+{
+    public static class ProvisioningServiceSingleton
+    {
+        public static IProvisioningService Instance { get; private set; }
+
+        public static void Initialize(IProvisioningService instance)
+        {
+            Instance = instance;
+        }
+    }
+}
