4f7b9654ad2150be464748120807622717415487
Matthias Mueller <matthiasm@live.de>
2018-09-05 11:20:33 +0200

    WIP

--- Sources/Application/Areas/Rest/Services/Implementation/RestProxy.cs
+++ Sources/Application/Areas/Rest/Services/Implementation/RestProxy.cs
@@ -22,7 +22,7 @@ namespace Mmu.Mlh.ApplicationExtensions.Areas.Rest.Services.Implementation
 
             using (var httpClient = new HttpClient())
             {
-                var request = CreateRequest(requestUri, restCall.Body, restCall.SecurityOptions);
+                var request = CreateRequest(requestUri, restCall.MethodType, restCall.Body, restCall.SecurityOptions);
 
                 using (var response = await httpClient.SendAsync(request))
                 {
@@ -39,17 +39,19 @@ namespace Mmu.Mlh.ApplicationExtensions.Areas.Rest.Services.Implementation
             }
         }
 
-        private static HttpRequestMessage CreateRequest(Uri requestUri, Maybe<object> body, SecurityOptions securityOptions)
+        private static HttpRequestMessage CreateRequest(Uri requestUri, RestCallMethodType methodType, Maybe<object> body, SecurityOptions securityOptions)
         {
-            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            var httpRequestMessage = new HttpRequestMessage(MapHttpMethod(methodType), requestUri);
 
             body.Evaluate(
                 bodyObj =>
                 {
                     var jsonBody = JsonConvert.SerializeObject(bodyObj);
                     httpRequestMessage.Content = new StringContent(jsonBody);
-                });
 
+                    httpRequestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                });
+            
             if (!securityOptions.ApplySecurity)
             {
                 return httpRequestMessage;
@@ -60,6 +62,19 @@ namespace Mmu.Mlh.ApplicationExtensions.Areas.Rest.Services.Implementation
             httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue("Basic", encodedCredentials);
 
             return httpRequestMessage;
+        }
+
+        private static HttpMethod MapHttpMethod(RestCallMethodType methodType)
+        {
+            switch (methodType)
+            {
+                case RestCallMethodType.Get:
+                    return HttpMethod.Get;
+                case RestCallMethodType.Post:
+                    return HttpMethod.Post;
+                default:
+                    throw new ArgumentException($"Invalid RestCallMethodType{ methodType }.");
+            }
         }
 
         private static T ParseResultContent<T>(string content)
