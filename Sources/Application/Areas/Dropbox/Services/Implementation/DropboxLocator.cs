31c77718d0fb2309f76d6a09f46321d60904deff
Matthias Müller <matthias.mueller@trivadis.com>
2020-07-02 21:02:15 +0200

    WIP

--- Sources/Application/Areas/Dropbox/Services/Implementation/DropboxLocator.cs
+++ Sources/Application/Areas/Dropbox/Services/Implementation/DropboxLocator.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.IO.Abstractions;
+using Mmu.Mlh.LanguageExtensions.Areas.Types.FunctionsResults;
 using Mmu.Mlh.LanguageExtensions.Areas.Types.Maybes;
 
 namespace Mmu.Mlh.ApplicationExtensions.Areas.Dropbox.Services.Implementation
 {
     internal class DropboxLocator : IDropboxLocator
     {
+        private const string DropboxInfoPath = @"Dropbox\info.json";
+
         private readonly IFileSystem _fileSystem;
 
         public DropboxLocator(IFileSystem fileSystem)
@@ -15,34 +18,54 @@ namespace Mmu.Mlh.ApplicationExtensions.Areas.Dropbox.Services.Implementation
 
         public Maybe<string> LocateDropboxPath()
         {
-            const string DropboxInfoPath = @"Dropbox\info.json";
-            var appDataPath = GetAppDataPath();
+            var appDataPathResult = TryGettingAppDataPath();
 
-            if (string.IsNullOrEmpty(appDataPath))
+            if (appDataPathResult.IsSuccess)
             {
-                var dropboxPathViaSystemVariable = GetDropboxPathViaSystemVariable();
-                if (string.IsNullOrEmpty(dropboxPathViaSystemVariable))
-                {
-                    return Maybe.CreateNone<string>();
-                }
+                var dropboxPath = _fileSystem.File.ReadAllText(appDataPathResult.Value).Split('\"')[5].Replace(@"\\", @"\");
 
-                return dropboxPathViaSystemVariable;
+                return dropboxPath;
             }
 
-            var dropboxInfoFullPath = _fileSystem.Path.Combine(appDataPath, DropboxInfoPath);
-            var dropboxPath = _fileSystem.File.ReadAllText(dropboxInfoFullPath).Split('\"')[5].Replace(@"\\", @"\");
-            return dropboxPath;
-        }
+            // Fallback, check if system variable is set
+            var dropboxPathViaSystemVariable = GetDropboxPathViaSystemVariable();
 
-        private static string GetAppDataPath()
-        {
-            var localAppDataPath = Environment.GetEnvironmentVariable("LocalAppData");
-            return string.IsNullOrEmpty(localAppDataPath) ? Environment.GetEnvironmentVariable("AppData") : localAppDataPath;
+            if (string.IsNullOrEmpty(dropboxPathViaSystemVariable))
+            {
+                return Maybe.CreateNone<string>();
+            }
+
+            return dropboxPathViaSystemVariable;
         }
 
         private static string GetDropboxPathViaSystemVariable()
         {
             return Environment.GetEnvironmentVariable("DropboxPath", EnvironmentVariableTarget.Machine);
+        }
+
+        private FunctionResult<string> TryCreatingDropboxPath(string environmentVariableName)
+        {
+            var envValue = Environment.GetEnvironmentVariable(environmentVariableName);
+            var dropboxInfoFullPath = _fileSystem.Path.Combine(envValue, DropboxInfoPath);
+
+            if (_fileSystem.File.Exists(dropboxInfoFullPath))
+            {
+                return FunctionResult.CreateSuccess(dropboxInfoFullPath);
+            }
+
+            return FunctionResult.CreateFailure<string>();
+        }
+
+        private FunctionResult<string> TryGettingAppDataPath()
+        {
+            var checkResult = TryCreatingDropboxPath("LocalAppData");
+
+            if (!checkResult.IsSuccess)
+            {
+                checkResult = TryCreatingDropboxPath("AppData");
+            }
+
+            return checkResult;
         }
     }
 }
