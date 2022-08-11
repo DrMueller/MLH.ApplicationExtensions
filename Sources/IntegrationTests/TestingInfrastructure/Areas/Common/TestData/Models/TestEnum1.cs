ba20afc0240937be616708061502b6b8641c883f
Matthias Müller <matthias.mueller@trivadis.com>
2022-08-11 13:58:13 +0200

    WIP

--- Sources/IntegrationTests/TestingInfrastructure/Areas/Common/TestData/Models/TestEnum1.cs
+++ Sources/IntegrationTests/TestingInfrastructure/Areas/Common/TestData/Models/TestEnum1.cs
@@ -1,9 +1,13 @@
-﻿namespace Mmu.Mlh.ApplicationExtensions.IntegrationTests.TestingInfrastructure.Areas.Common.TestData.Models
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Mmu.Mlh.ApplicationExtensions.IntegrationTests.TestingInfrastructure.Areas.Common.TestData.Models
 {
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Enumeration items should be documented", Justification = "should remove rule")]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1602:Enumeration items should be documented", Justification = "should remove rule")]
     public enum TestEnum1
     {
         Value0 = 0,
         Value1 = 1,
-        Value2 = 2
+        Value2 = 2,
     }
 }
